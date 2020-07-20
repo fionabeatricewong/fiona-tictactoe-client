17053eda49f658fded50e699409ee63736c7733d
fionabeatricewong <fionabeatricewong@gmail.com>
2020-07-20 13:44:06 -0400

    Finished tracking board for create game in JS

--- assets/scripts/game/ui.js
+++ assets/scripts/game/ui.js
@@ -3,6 +3,25 @@
 // Require:
 const store = require('../store')
 
-module.exports = {
+$('#game-started').hide()
 
+// Create Game:
+const createGameSuccess = function (response) {
+  $('#message').text('Game started.')
+
+  $('#game-started').show()
+
+  store.gameState = {
+    board: response.game.cells,
+    over: response.game.over
+  }
+}
+
+const createGameFailure = function () {
+  $('#message').text('Game not started.')
+}
+
+module.exports = {
+  createGameSuccess,
+  createGameFailure
 }
